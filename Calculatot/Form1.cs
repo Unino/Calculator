881b711524d6caa30aebe6ccdb27a823d9210294
unino <phuongtrang06@gmail.com>
2024-09-19 17:05:02 +0700

    Them chuc nang load

--- Calculatot/Form1.cs
+++ Calculatot/Form1.cs
@@ -6,5 +6,10 @@ namespace Calculatot
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("asf");
+        }
     }
 }
