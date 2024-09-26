d155e9a6da56e71d5a0ba357107b1ea0c4d6c394
unino <phuongtrang06@gmail.com>
2024-09-26 14:17:48 +0700

    Xu ly conflit2

--- Calculatot/Form1.Designer.cs
+++ Calculatot/Form1.Designer.cs
@@ -30,15 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-<<<<<<< HEAD
             this.textBox2 = new System.Windows.Forms.TextBox();
-=======
->>>>>>> feature-cal
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(352, 225);
+            this.button1.Location = new System.Drawing.Point(348, 227);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -48,16 +45,11 @@
             // 
             // textBox1
             // 
-<<<<<<< HEAD
             this.textBox1.Location = new System.Drawing.Point(335, 103);
-=======
-            this.textBox1.Location = new System.Drawing.Point(334, 98);
->>>>>>> feature-cal
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 1;
             // 
-<<<<<<< HEAD
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(335, 170);
@@ -65,17 +57,12 @@
             this.textBox2.Size = new System.Drawing.Size(100, 23);
             this.textBox2.TabIndex = 1;
             // 
-=======
->>>>>>> feature-cal
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-<<<<<<< HEAD
             this.Controls.Add(this.textBox2);
-=======
->>>>>>> feature-cal
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -90,9 +77,7 @@
 
         private Button button1;
         private TextBox textBox1;
-<<<<<<< HEAD
         private TextBox textBox2;
-=======
->>>>>>> feature-cal
+
     }
 }
