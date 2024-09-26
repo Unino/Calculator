9b19ae70dda82a06b6c36118b35bb75f25558cae
unino <phuongtrang06@gmail.com>
2024-09-26 15:48:58 +0700

    Thay doi giao dien dau cong

--- Calculatot/Form1.Designer.cs
+++ Calculatot/Form1.Designer.cs
@@ -36,9 +36,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(360, 310);
+            this.button1.Location = new System.Drawing.Point(229, 284);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(312, 96);
             this.button1.TabIndex = 0;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
